8303b69763597ee1fbc6235cdb53842b956f3c3a
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-05-26 23:16:57 +0200

    Set owners to getAmiOutput using getCallerIdentity

--- aws/launch-template.ts
+++ aws/launch-template.ts
@@ -8,25 +8,27 @@ import { Buffer } from 'buffer';
 const config = new pulumi.Config();
 const size = config.require("machineType");
 
-export const launchTemplate = startupScript.then(script => {
-    const ami = aws.ec2.getAmiOutput({
-        filters: [{
-            name: "name",
-            values: [ config.require("machineImage") ],
-        }],
-        owners: ["099720109477"], // This owner ID is Amazon
-    });
-
-    const launchTemplateArgs: LaunchTemplateArgs = {
-        namePrefix: "ghrunner-asg",
-        imageId: ami.id,
-        instanceType: size,
-        keyName: "gr-oss-team",
-        vpcSecurityGroupIds: [ securityGroup.id ],
-        userData: Buffer.from(script).toString("base64"),
-        tags: {
-            "Name": "Github Runner"
+export const launchTemplate = aws.getCallerIdentity({}).then(identity => {
+    return startupScript.then(script => {
+        const ami = aws.ec2.getAmiOutput({
+            filters: [{
+                name: "name",
+                values: [ config.require("machineImage") ],
+            }],
+            owners: [identity.accountId],
+        });
+    
+        const launchTemplateArgs: LaunchTemplateArgs = {
+            namePrefix: "ghrunner-asg",
+            imageId: ami.id,
+            instanceType: size,
+            keyName: "gr-oss-team",
+            vpcSecurityGroupIds: [ securityGroup.id ],
+            userData: Buffer.from(script).toString("base64"),
+            tags: {
+                "Name": "Github Runner"
+            }
         }
-    }
-    return new aws.ec2.LaunchTemplate("ghrunner-lt", launchTemplateArgs);
+        return new aws.ec2.LaunchTemplate("ghrunner-lt", launchTemplateArgs);
+    });
 });
