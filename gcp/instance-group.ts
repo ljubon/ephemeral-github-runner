a35142245fdc792c2adf294e25c3deac6b3fccf7
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-05-29 23:03:24 +0200

    Add autoscaler to GCP instance group

--- gcp/instance-group.ts
+++ gcp/instance-group.ts
@@ -12,3 +12,12 @@ export const instanceGroup = new gcp.compute.InstanceGroupManager(`${baseName}-i
         instanceTemplate: instanceTemplate.id,
     }],
 });
+
+const defaultAutoscaler = new gcp.compute.Autoscaler(`${baseName}-autoscaler`, {
+    zone: process.env.GOOGLE_ZONE,
+    target: instanceGroup.id,
+    autoscalingPolicy: {
+        maxReplicas: config.requireNumber("runnersCount"),
+        minReplicas: config.requireNumber("runnersCount"),
+    },
+});
