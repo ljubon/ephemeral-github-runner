91b8d9d3af98690691e38280b5c0397c04676512
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-07-22 21:43:31 +0200

    revert to single instances without autoscaling group and templates

--- aws/index.ts
+++ aws/index.ts
@@ -1,3 +1,5 @@
-import { autoscalingGroup } from './autoscaling-group';
+// import { autoscalingGroup } from './autoscaling-group';
+import { instance } from './instance';
 
-export const autoscalingGroupArn = autoscalingGroup.then(asg => asg.arn);
+// export const autoscalingGroupArn = autoscalingGroup.then(asg => asg.arn);
+export const instanceArn = instance.then(i => i.arn); 
---
+++ aws/instance.ts
@@ -0,0 +1,38 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { startupScript } from '../startupScript';
+import { securityGroup } from './security-group';
+
+const config = new pulumi.Config();
+const machineType = config.require("machineType");
+
+export const instance = aws.getCallerIdentity({}).then(identity => {
+    return startupScript.then(script => {
+        const ami = aws.ec2.getAmiOutput({
+            filters: [{
+                name: "name",
+                values: [ config.require("machineImage") ],
+            }],
+            owners: [identity.accountId],
+        });
+
+        const runnerInstance = new aws.ec2.Instance("ghrunner", {
+            monitoring: true,
+            ami: ami.id,
+            instanceType: machineType,
+            keyName: "gross-devops-us-east-1",
+            tags: {
+                "Name": "Github Runner"
+            },
+            vpcSecurityGroupIds: [ securityGroup.id ],
+            userData: Buffer.from(script).toString("base64"),
+            ebsBlockDevices: [{
+                deviceName: "/dev/sda1",
+                volumeSize: config.requireNumber("bootDiskSizeInGB"),
+                volumeType: config.require("bootDiskType")
+            }],
+        }); 
+
+        return runnerInstance;
+    });
+})
