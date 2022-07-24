81857ac8f79947ddbfd957cc612e14ecdec11bd3
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-07-24 21:00:21 +0200

    Remove ingress rules. Refactor Name tag

--- aws/index.ts
+++ aws/index.ts
@@ -1,5 +1,3 @@
-// import { autoscalingGroup } from './autoscaling-group';
 import { instance } from './instance';
 
-// export const autoscalingGroupArn = autoscalingGroup.then(asg => asg.arn);
 export const instanceId = instance.then(i => i.id);
