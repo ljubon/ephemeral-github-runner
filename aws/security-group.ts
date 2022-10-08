1f3f6a8562c7d13ab11cdfa5891098f2f0d14316
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-10-08 11:14:32 +0200

    Remove trailing whitespace

--- aws/security-group.ts
+++ aws/security-group.ts
@@ -7,7 +7,7 @@ export const securityGroup = new aws.ec2.SecurityGroup("ghrunner-sg", {
     egress: [
         { protocol: "tcp", fromPort: 0, toPort: 65535, cidrBlocks: ["0.0.0.0/0"] },
     ],
-    tags: { 
+    tags: {
         "Name": `${config.require("repo")} Github Runner`,
         "map-migrated": "d-server-01068mdjl5jze3"
     },
